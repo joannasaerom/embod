5d02368139cbf8bc49467b3469c05e88c51b65d6
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-01-02 15:48:59 -0500

    Add submenu to services pages

--- app/app.php
+++ app/app.php
@@ -37,35 +37,75 @@
         $title = "Services";
         $imgurl = "/assets/img/tech3.jpg";
         $content = "Cow taleggio boursin. Parmesan cheeseburger goat stinking bishop pecorino parmesan bocconcini cheese on toast. Dolcelatte blue castello fromage frais queso red leicester cauliflower cheese caerphilly danish fontina. Boursin smelly cheese cottage cheese stilton cheese on toast port-salut cheese triangles say cheese. Cheese and biscuits emmental camembert de normandie feta cheese and biscuits everyone loves blue castello croque monsieur. Emmental mascarpone cut the cheese smelly cheese the big cheese taleggio port-salut who moved my cheese. Red leicester st. agur blue cheese paneer halloumi cheeseburger everyone loves cream cheese stilton. Who moved my cheese brie taleggio jarlsberg manchego everyone loves cheddar cut the cheese. Manchego cheeseburger cheese slices camembert de normandie cheeseburger pepper jack parmesan fondue. When the cheese comes out everybody's happy airedale melted cheese macaroni cheese blue castello gouda manchego monterey jack. The big cheese mascarpone babybel camembert de normandie cream cheese roquefort red leicester babybel. Cheese strings mascarpone bavarian bergkase cheeseburger cottage cheese.";
-        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+        $submenus = array(
+          'Services' => '/services',
+          'Glove Box' => '/glovebox',
+          'Parylene' => '/parylene',
+          'Plasma' => '/plasma',
+          'Sputtering System' => '/sputteringsystem'
+        );
+
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content, 'submenus' => $submenus));
     });
 
     $app->get("/glovebox", function() use ($app) {
         $title = "Glove Box";
         $imgurl = "/assets/img/tech3.jpg";
         $content = "Cow taleggio boursin. Parmesan cheeseburger goat stinking bishop pecorino parmesan bocconcini cheese on toast. Dolcelatte blue castello fromage frais queso red leicester cauliflower cheese caerphilly danish fontina. Boursin smelly cheese cottage cheese stilton cheese on toast port-salut cheese triangles say cheese. Cheese and biscuits emmental camembert de normandie feta cheese and biscuits everyone loves blue castello croque monsieur. Emmental mascarpone cut the cheese smelly cheese the big cheese taleggio port-salut who moved my cheese. Red leicester st. agur blue cheese paneer halloumi cheeseburger everyone loves cream cheese stilton. Who moved my cheese brie taleggio jarlsberg manchego everyone loves cheddar cut the cheese. Manchego cheeseburger cheese slices camembert de normandie cheeseburger pepper jack parmesan fondue. When the cheese comes out everybody's happy airedale melted cheese macaroni cheese blue castello gouda manchego monterey jack. The big cheese mascarpone babybel camembert de normandie cream cheese roquefort red leicester babybel. Cheese strings mascarpone bavarian bergkase cheeseburger cottage cheese.";
-        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+        $submenus = array(
+          'Services' => '/services',
+          'Glove Box' => '/glovebox',
+          'Parylene' => '/parylene',
+          'Plasma' => '/plasma',
+          'Sputtering System' => '/sputteringsystem'
+        );
+
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content, 'submenus' => $submenus));
     });
 
     $app->get("/parylene", function() use ($app) {
         $title = "Parylene";
         $imgurl = "/assets/img/tech3.jpg";
         $content = "Cow taleggio boursin. Parmesan cheeseburger goat stinking bishop pecorino parmesan bocconcini cheese on toast. Dolcelatte blue castello fromage frais queso red leicester cauliflower cheese caerphilly danish fontina. Boursin smelly cheese cottage cheese stilton cheese on toast port-salut cheese triangles say cheese. Cheese and biscuits emmental camembert de normandie feta cheese and biscuits everyone loves blue castello croque monsieur. Emmental mascarpone cut the cheese smelly cheese the big cheese taleggio port-salut who moved my cheese. Red leicester st. agur blue cheese paneer halloumi cheeseburger everyone loves cream cheese stilton. Who moved my cheese brie taleggio jarlsberg manchego everyone loves cheddar cut the cheese. Manchego cheeseburger cheese slices camembert de normandie cheeseburger pepper jack parmesan fondue. When the cheese comes out everybody's happy airedale melted cheese macaroni cheese blue castello gouda manchego monterey jack. The big cheese mascarpone babybel camembert de normandie cream cheese roquefort red leicester babybel. Cheese strings mascarpone bavarian bergkase cheeseburger cottage cheese.";
-        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+        $submenus = array(
+          'Services' => '/services',
+          'Glove Box' => '/glovebox',
+          'Parylene' => '/parylene',
+          'Plasma' => '/plasma',
+          'Sputtering System' => '/sputteringsystem'
+        );
+
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content, 'submenus' => $submenus));
     });
 
     $app->get("/plasma", function() use ($app) {
         $title = "Plasma";
         $imgurl = "/assets/img/tech3.jpg";
         $content = "Cow taleggio boursin. Parmesan cheeseburger goat stinking bishop pecorino parmesan bocconcini cheese on toast. Dolcelatte blue castello fromage frais queso red leicester cauliflower cheese caerphilly danish fontina. Boursin smelly cheese cottage cheese stilton cheese on toast port-salut cheese triangles say cheese. Cheese and biscuits emmental camembert de normandie feta cheese and biscuits everyone loves blue castello croque monsieur. Emmental mascarpone cut the cheese smelly cheese the big cheese taleggio port-salut who moved my cheese. Red leicester st. agur blue cheese paneer halloumi cheeseburger everyone loves cream cheese stilton. Who moved my cheese brie taleggio jarlsberg manchego everyone loves cheddar cut the cheese. Manchego cheeseburger cheese slices camembert de normandie cheeseburger pepper jack parmesan fondue. When the cheese comes out everybody's happy airedale melted cheese macaroni cheese blue castello gouda manchego monterey jack. The big cheese mascarpone babybel camembert de normandie cream cheese roquefort red leicester babybel. Cheese strings mascarpone bavarian bergkase cheeseburger cottage cheese.";
-        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+        $submenus = array(
+          'Services' => '/services',
+          'Glove Box' => '/glovebox',
+          'Parylene' => '/parylene',
+          'Plasma' => '/plasma',
+          'Sputtering System' => '/sputteringsystem'
+        );
+
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content, 'submenus' => $submenus));
     });
 
     $app->get("/sputteringsystem", function() use ($app) {
         $title = "Sputtering System";
         $imgurl = "/assets/img/tech3.jpg";
         $content = "Cow taleggio boursin. Parmesan cheeseburger goat stinking bishop pecorino parmesan bocconcini cheese on toast. Dolcelatte blue castello fromage frais queso red leicester cauliflower cheese caerphilly danish fontina. Boursin smelly cheese cottage cheese stilton cheese on toast port-salut cheese triangles say cheese. Cheese and biscuits emmental camembert de normandie feta cheese and biscuits everyone loves blue castello croque monsieur. Emmental mascarpone cut the cheese smelly cheese the big cheese taleggio port-salut who moved my cheese. Red leicester st. agur blue cheese paneer halloumi cheeseburger everyone loves cream cheese stilton. Who moved my cheese brie taleggio jarlsberg manchego everyone loves cheddar cut the cheese. Manchego cheeseburger cheese slices camembert de normandie cheeseburger pepper jack parmesan fondue. When the cheese comes out everybody's happy airedale melted cheese macaroni cheese blue castello gouda manchego monterey jack. The big cheese mascarpone babybel camembert de normandie cream cheese roquefort red leicester babybel. Cheese strings mascarpone bavarian bergkase cheeseburger cottage cheese.";
-        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+        $submenus = array(
+          'Services' => '/services',
+          'Glove Box' => '/glovebox',
+          'Parylene' => '/parylene',
+          'Plasma' => '/plasma',
+          'Sputtering System' => '/sputteringsystem'
+        );
+
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content, 'submenus' => $submenus));
     });
 
     $app->get("/ascenter", function() use ($app) {
