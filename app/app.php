347b7bc45ae5cb63a034ccda4f9d7ce90b76bb1f
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-01-02 13:23:16 -0500

    add sass file

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+?>
