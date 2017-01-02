abd830af92fe2cddff1639905ed3c38e31dc9965
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-01-02 15:22:22 -0500

    Add http get request to pages

--- app/app.php
+++ app/app.php
@@ -11,5 +11,64 @@
         return $app['twig']->render('index.html.twig');
     });
 
+    $app->get("/about", function() use ($app) {
+        $title = "Testing";
+        $imgurl = "/assets/img/tech3.jpg";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+    });
+
+    $app->get("/ceomessage", function() use ($app) {
+        $title = "Testing";
+        $imgurl = "/assets/img/tech3.jpg";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+    });
+
+    $app->get("/services", function() use ($app) {
+        $title = "Testing";
+        $imgurl = "/assets/img/tech3.jpg";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+    });
+
+    $app->get("/glovebox", function() use ($app) {
+        $title = "Testing";
+        $imgurl = "/assets/img/tech3.jpg";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+    });
+
+    $app->get("/parylene", function() use ($app) {
+        $title = "Testing";
+        $imgurl = "/assets/img/tech3.jpg";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+    });
+
+    $app->get("/plasma", function() use ($app) {
+        $title = "Testing";
+        $imgurl = "/assets/img/tech3.jpg";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+    });
+
+    $app->get("/sputteringsystem", function() use ($app) {
+        $title = "Testing";
+        $imgurl = "/assets/img/tech3.jpg";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'imgurl' => $imgurl, 'content' => $content));
+    });
+
+    $app->get("/ascenter", function() use ($app) {
+        $title = "Testing";
+        $content = "blahblahblah";
+        return $app['twig']->render('three-col-subpage.html.twig', array('title' => $title, 'content' => $content));
+    });
+
+    $app->get("/contact", function() use ($app) {
+        return $app['twig']->render('contact.html.twig');
+    });
+
     return $app;
 ?>
