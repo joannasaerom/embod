377eb6faf4b2212a441945e4ade84a27c8f313fd
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-01-03 13:52:04 -0500

    Add text below our services banner

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -3,20 +3,21 @@ $(document).ready(function() {
   $("#rectangle").addClass("reveal");
   $("#companyName").addClass("revealText");
   $("#companySlogan").addClass("revealText");
+  $("#servicesButton").addClass("revealText");
 });
 
 
-// $(window).on("load resize",function(e) {
-//
-//   var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
-//   $("#js-centered-navigation-menu").removeClass("show");
-//
-//   menuToggle.on("click", function(e) {
-//     e.preventDefault();
-//     $("#js-centered-navigation-menu").slideToggle(function(){
-//       if($("#js-centered-navigation-menu").is(":hidden")) {
-//         $("#js-centered-navigation-menu").removeAttr("style");
-//       }
-//     });
-//   });
-// });
+$(window).on("load resize",function(e) {
+
+  var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
+  $("#js-centered-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-centered-navigation-menu").slideToggle(function(){
+      if($("#js-centered-navigation-menu").is(":hidden")) {
+        $("#js-centered-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+});
