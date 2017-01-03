f3281d74d0bd417c8327ea13498262791e90ac39
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-01-03 14:26:28 -0500

    Add smooth scroll to services button

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -6,6 +6,29 @@ $(document).ready(function() {
   $("#servicesButton").addClass("revealText");
 });
 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// $(".servicesButton a[href^='#']").click(function(e){
+//   e.preventDefault();
+//
+//   $("html, body").stop().animate({
+//     "scrollTop": $(this.hash).offset().top
+//   }, 900, "swing");
+// });
+
 
 $(window).on("load resize",function(e) {
 
