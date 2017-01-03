43a804f00be4bc0f46fccb00d043a2d5da87fea5
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-01-03 18:05:21 -0500

    Separate sass into separate files and folders

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -1,37 +1,25 @@
-
-
 $(document).ready(function() {
   $("#rectangle").addClass("reveal");
   $("#companyName").addClass("revealText");
   $("#companySlogan").addClass("revealText");
   $("#servicesButton").addClass("revealText");
 
-});
-
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
+    });
   });
+
 });
-
-
-// $(".servicesButton a[href^='#']").click(function(e){
-//   e.preventDefault();
-//
-//   $("html, body").stop().animate({
-//     "scrollTop": $(this.hash).offset().top
-//   }, 900, "swing");
-// });
-
 
 $(window).on("load resize",function(e) {
 
